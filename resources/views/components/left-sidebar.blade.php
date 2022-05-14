6aebc3de1850ede7280602ef2d2a5eeb786070e1
Boy Prakasa <boyprakasa@gmail.com>
2022-05-14 12:39:59 +0700

    left menu with role and permission

--- resources/views/components/left-sidebar.blade.php
+++ resources/views/components/left-sidebar.blade.php
@@ -4,30 +4,69 @@
             <ul id="sidebarnav">
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('home') }}"
-                        aria-expanded="false"><i data-feather="home"></i><span class="hide-menu">Dashboard</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('informasi.index') }}"
                         aria-expanded="false">
-                        <i data-feather="info"></i><span class="hide-menu">Informasi</span>
+                        <i class="fas fa-tachometer-alt"></i>
+                        <span class="hide-menu">Dashboard</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('download.index') }}"
-                        aria-expanded="false">
-                        <i data-feather="download"></i><span class="hide-menu">Download</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('admin.index') }}"
-                        aria-expanded="false">
-                        <i data-feather="user"></i><span class="hide-menu">Admin</span>
-                    </a>
-                </li>
+
+                @can('N01', 'N02', 'N03')
+                    <li class="sidebar-item">
+                        <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
+                            aria-expanded="false">
+                            <i data-feather="settings"></i>
+                            <span class="hide-menu">Permohonan</span>
+                        </a>
+                        <ul aria-expanded="false" class="collapse first-level">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                    <i class="mdi mdi-octagram"></i>
+                                    <span class="hide-menu"> Baru</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                    <i class="mdi mdi-key"></i>
+                                    <span class="hide-menu"> Revisi</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="javascript:void(0)" class="sidebar-link">
+                                    <i class="mdi mdi-playlist-check"></i>
+                                    <span class="hide-menu"> Proses</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endcan
+
+                @role('Super Admin|Admin')
+                    <li class="sidebar-item">
+                        <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
+                            aria-expanded="false">
+                            <i class="fas fa-users"></i>
+                            <span class="hide-menu">Pemohon</span>
+                        </a>
+                        <ul aria-expanded="false" class="collapse first-level">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                    <i class="mdi mdi-octagram"></i>
+                                    <span class="hide-menu"> Akun</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="javascript:void(0)" class="sidebar-link">
+                                    <i class="mdi mdi-octagram"></i>
+                                    <span class="hide-menu"> Profil</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endrole
+
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="#" aria-expanded="false"><i
-                            data-feather="book"></i><span class="hide-menu">Menu 1</span>
+                            data-feather="book"></i><span class="hide-menu">Laporan</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
@@ -37,36 +76,63 @@
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="#" aria-expanded="false"><i
-                            data-feather="briefcase"></i><span class="hide-menu">Menu 3</span>
+                            data-feather="settings"></i><span class="hide-menu">Menu 3</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
-                        aria-expanded="false">
-                        <i data-feather="settings"></i>
-                        <span class="hide-menu">Dropdown</span>
-                    </a>
-                    <ul aria-expanded="false" class="collapse first-level">
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">
-                                <i class="mdi mdi-octagram"></i>
-                                <span class="hide-menu"> Item 1</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="javascript:void(0)" class="sidebar-link">
-                                <i class="mdi mdi-playlist-check"></i>
-                                <span class="hide-menu"> Item 2</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">
-                                <i class="mdi mdi-key"></i>
-                                <span class="hide-menu"> Item 3</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+
+                @role('Super Admin|Admin')
+                    <div class="dropdown-divider"></div>
+
+                    <li class="sidebar-item">
+                        <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('informasi.index') }}"
+                            aria-expanded="false">
+                            <i data-feather="info"></i><span class="hide-menu">Informasi</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('download.index') }}"
+                            aria-expanded="false">
+                            <i data-feather="download"></i><span class="hide-menu">Download</span>
+                        </a>
+                    </li>
+                @endrole
+
+                @role('Super Admin')
+                    <li class="sidebar-item">
+                        <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
+                            aria-expanded="false">
+                            <i data-feather="briefcase"></i>
+                            <span class="hide-menu">Master</span>
+                        </a>
+                        <ul aria-expanded="false" class="collapse first-level">
+                            <li class="sidebar-item">
+                                <a href="{{ route('admin.index') }}" class="sidebar-link">
+                                    <i class="mdi mdi-octagram"></i>
+                                    <span class="hide-menu">Admin</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                    <i class="mdi mdi-octagram"></i>
+                                    <span class="hide-menu"> Item 1</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="javascript:void(0)" class="sidebar-link">
+                                    <i class="mdi mdi-playlist-check"></i>
+                                    <span class="hide-menu"> Item 2</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                    <i class="mdi mdi-key"></i>
+                                    <span class="hide-menu"> Item 3</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endrole
+
             </ul>
         </nav>
     </div>
